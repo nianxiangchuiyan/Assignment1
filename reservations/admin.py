39c9fc0c582affde9122fa4462464b45643e771e
nianxiangchuiyan <161560763+nianxiangchuiyan@users.noreply.github.com>
2025-04-26 15:26:48 +1200

    Revert "version 1: booking"
    This reverts commit 785358a689622c6447d287c1cbad756ee6ae0f5f.

--- reservations/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Room, Reservation, CustomUser
-
-admin.site.register(Room)
-admin.site.register(Reservation)
-admin.site.register(CustomUser)
