0778b0231aa1eb0dd8f56b7b9ef5f504cc239baf
nianxiangchuiyan <161560763+nianxiangchuiyan@users.noreply.github.com>
2025-04-30 16:57:27 +1200

    vercel（bug fix）

--- Assignment1/wsgi.py
+++ Assignment1/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
+from django.core.asgi import get_asgi_application
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Assignment1.settings')
 
-application = get_wsgi_application()
+app = get_asgi_application()
--- vercel_handler.py
+++ vercel_handler.py
@@ -2,5 +2,5 @@ import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Assignment1.settings")
-application = get_asgi_application()
+app = get_asgi_application()
 print("Django loaded from vercel_handler")
