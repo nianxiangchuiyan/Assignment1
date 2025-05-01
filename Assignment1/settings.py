a029e636f6e8db757e362d561c43ee968e240b3f
nianxiangchuiyan <161560763+nianxiangchuiyan@users.noreply.github.com>
2025-05-01 14:19:40 +1200

    vercel（allow sql）

--- Assignment1/settings.py
+++ Assignment1/settings.py
@@ -13,6 +13,8 @@ import os
 from operator import truediv
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,11 +80,17 @@ WSGI_APPLICATION = 'Assignment1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+#
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
