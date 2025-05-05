c8c8e798bf10b65e85ebfb71a77dbb26aa7265af
nianxiangchuiyan <161560763+nianxiangchuiyan@users.noreply.github.com>
2025-05-05 17:22:49 +1200

    vercel（deploy admin css）

--- Assignment1/wsgi.py
+++ Assignment1/wsgi.py
@@ -9,10 +9,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
+from whitenoise import WhiteNoise
+
 from django.core.asgi import get_asgi_application
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Assignment1.settings')
 
-app = get_asgi_application()
+
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'static'))
+app = get_asgi_application()
